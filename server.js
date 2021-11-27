81312458c3ede8e58ea13a991d13760d1242e5c9
Mussie <mussie.fitsum@gmail.com>
2021-11-27 14:15:22 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use('/users/', require('./routes/usersRoute'));
 
 app.listen(3001, () => {
     console.log('Express Listening on Port 3000');
