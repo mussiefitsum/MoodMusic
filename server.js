95fb6db3a5c65b8e551b6655419ae170611d17d5
Mussie <mussie.fitsum@gmail.com>
2021-11-27 14:22:23 -0500

    fixed port

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
 const express = require('express');
 const app = express();
 
 
-app.listen(3001, () => {
-    console.log('Express Listening on Port 3000');
-})
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Listening on Port ${ port }`);
+});
