b6a3b7c0c5c5c5733279fb7a683fcb9466785219
Mussie <mussie.fitsum@gmail.com>
2021-11-27 14:36:14 -0500

    connecting server to react

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ if (process.env.NODE_ENV !== "production") {
 const express = require('express');
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, './front-end/build')));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './front-end/build', 'index.html'));
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
